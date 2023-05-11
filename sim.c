b776d7149fee16c06aaa46588c751412d9575f59
asaladi <122575267+asaladi@users.noreply.github.com>
2023-05-10 18:36:21 -0700

    Update sim.c

--- sim.c
+++ sim.c
@@ -23,7 +23,33 @@ size_t getModVal(int x, size_t N){
  * process one row of the board
  */
 static void doRow(belem *dest, belem *src, size_t row, size_t rows, size_t cols){
-  // TODO: 
+  int check_cell = 0;
+  int neighbor_cell = 0;
+  for(int i = 0; i< (int)cols; i++){
+    check_cell = getIndex(cols,row,i);
+    int alive_count = 0;
+    
+    for(int r = -1; r<=1; r++){
+      for(int c = -1; c<=1; c++){
+        neighbor_cell = getModVal(getModVal(check_cell+c,cols)+(r*cols),rows*cols);
+
+        if (*(src+neighbor_cell)==1){
+          alive_count++;
+        }
+      } 
+    }
+
+    if (*(src+check_cell)==1){
+      if(alive_count!=2 && alive_count!=3){
+        *(dest+check_cell)=0;
+      }
+    }
+    else{
+      if(alive_count==3){
+        *(dest+check_cell)=1;
+      }
+    }
+  }
 }
 
 
@@ -35,5 +61,10 @@ static void doRow(belem *dest, belem *src, size_t row, size_t rows, size_t cols)
  *   swap current and next
  */
 void simLoop(boards_t *self, unsigned int steps){
-  // TODO:
+  for(int i = 0; i<(int)steps; i++){
+    for(int r = 0; r<(int)(self->numRows); r++){
+      doRow(self->bufferA, self->bufferB, (size_t)r, self->numRows, self->numCols);
+    }
+    swapBuffers(self);
+  }
 }
